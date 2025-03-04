50afe9b273b5627994e7898d5390a631cf154b00
jatin kakadiy <jatinkakadiya234@gmail.com>
2025-03-04 17:40:56 +0530

    db url change

--- config/DataBase.js
+++ config/DataBase.js
@@ -1,14 +1,16 @@
 const { default: mongoose } = require("mongoose");
 // require("dotenv").config()
 
-async function ConnectDb(){
+async function ConnectDb() {
     try {
-        await mongoose.connect("mongodb+srv://jatinkakadiya234:iqD6w9vBN1nOQuw5@cluster0.pmp8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        console.log("Db Conneted")
+        await mongoose.connect("mongodb+srv://jatinkakadiya234:iqD6w9vBN1nOQuw5@cluster0.pmp8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("Db Connected");
     } catch (error) {
-        console.log("Db Connection Loss")
+        console.error("Db Connection Error:", error.message);
+        setTimeout(ConnectDb, 5000); // 5 સેકન્ડ પછી ફરીથી Retry કરો
     }
 }
+
 
 
 module.exports = ConnectDb
