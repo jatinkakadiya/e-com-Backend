206fa56267a9e0e41a3be46f82556e9b2f3f9448
jatin kakadiy <jatinkakadiya234@gmail.com>
2025-03-05 13:56:46 +0530

    port add

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = expres()
 app.use(expres.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000',  // Frontend URL
+  origin: 'http://localhost:3000'|| "http://localhost:5173",  // Frontend URL
   credentials: true                // Allow cookies to be sent/received
 }));
 
