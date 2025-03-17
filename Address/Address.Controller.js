6715936f0e8cbf8f46637e233d507c0bdebfd6da
jatin kakadiy <jatinkakadiya234@gmail.com>
2025-03-17 13:31:56 +0530

    create api for address update and delete

--- Address/Address.Controller.js
+++ Address/Address.Controller.js
@@ -3,7 +3,7 @@ const addressModel = require("./Address.Model");
 const AddressController = {
     addAddress: async (req, res) => {
         try {
-            const { FristName, LastName, Phone, City, State, AddressName, Pincode ,user } = req.body
+            const { FristName, LastName, Phone, City, State, AddressName, Pincode, user } = req.body
             if (!FristName || !LastName || !Phone || !City || !State || !AddressName || !Pincode || !user) return res.status(404).send({ message: "missing dependecy" })
             const result = await addressModel.create({ ...req.body })
             if (!result) return res.status(500).send({ message: "somthing went wrong" })
@@ -15,8 +15,8 @@ const AddressController = {
     },
     ListAddress: async (req, res) => {
         try {
-            let  {user} = req.params
-            const result = await addressModel.find({user})
+            let { user } = req.params
+            const result = await addressModel.find({ user })
             if (!result) return res.status(500).send({ message: "somthing went wrong" })
             return res.status(200).send({ message: "sucsess", data: result })
         } catch (error) {
@@ -29,6 +29,27 @@ const AddressController = {
             const result = await addressModel.findById(id)
             if (!result) return res.status(500).send({ message: "somthing went wrong" })
             return res.status(200).send({ message: "sucsess", data: result })
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    DeleteAddess: async (req, res) => {
+        try {
+            const { id } = req.params
+            if (!id) return res.status(404).send({ nmessage: "missing dependency" })
+            let result = await addressModel.findByIdAndDelete({ _id: id })
+            if (!result) return res.status(500).send({ message: "somthing went wrong" })
+            return res.status(200).send({ message: "sucsess" })
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    UpdateAddress:async(req,res)=>{
+        try {
+            let  {id} = req.params
+            let result = await addressModel.findByIdAndUpdate({_id:id},{...req.body})
+            if (!result) return res.status(500).send({ message: "somthing went wrong" })
+                return res.status(200).send({ message: "sucsess" })
         } catch (error) {
             console.log(error);
         }
--- Address/Address.Router.js
+++ Address/Address.Router.js
@@ -5,5 +5,7 @@ const AddressRouter = express.Router()
 AddressRouter.post("/create",AddressController.addAddress)
 AddressRouter.get("/list/:user",AddressController.ListAddress)
 AddressRouter.get("/getBy/:id",AddressController.getAddressById)
+AddressRouter.delete("/delete/address/:id",AddressController.DeleteAddess)
+AddressRouter.put("/update/address/:id",AddressController.UpdateAddress)
 
 module.exports = AddressRouter
