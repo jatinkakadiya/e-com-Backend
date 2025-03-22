a5575d8322a9554da9a6fbfcf56e264af3018537
jatin kakadiy <jatinkakadiya234@gmail.com>
2025-03-22 11:42:20 +0530

    changese

--- app.js
+++ app.js
@@ -24,12 +24,6 @@ app.use(cors({
   credentials: true                // Allow cookies to be sent/received
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 ConnectDb()
 // ProductController.Insartmany()
