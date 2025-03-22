d6d07bf652e9917ae1d7e3783ffe9ca664e1bae1
jatin kakadiy <jatinkakadiya234@gmail.com>
2025-03-22 12:00:51 +0530

    cjnc

--- Product/ProductRoutes.js
+++ Product/ProductRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const ProductController = require("./ProductController")
+const Uploaded = require("../middleware/Multer")
 const {Auth,RoleAuth} = require("../middleware/Auth")
-const upload = require("../middleware/Multer")
 const produtrouter = express.Router()
 
-produtrouter.post("/create",upload.single("Image"),ProductController.CreateProduct)
+produtrouter.post("/create",Uploaded.single("Image"),ProductController.CreateProduct)
 produtrouter.get("/list",ProductController.ListProduct)
 produtrouter.delete("/delete/:id",ProductController.DeleteProduct)
 produtrouter.get("/productsbyid/:id",ProductController.GetproductById)
--- middleware/Multer.js
+++ middleware/Multer.js
@@ -1,15 +1,15 @@
 const multer = require("multer");
-const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "public/uploads"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const Store = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './Public')
+      },
+      filename: function (req, file, cb) {
+        
+        cb(null, file.originalname)
+      }
+})
 
-const upload = multer({ storage });
+const Uploaded = multer({storage:Store})
 
-module.exports = upload
+module.exports = Uploaded
